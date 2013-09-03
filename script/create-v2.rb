989ae849c8ba03b19b5208c1e5cdabbe96742e17
Tim Abell <tim@timwise.co.uk>
2013-09-03 17:41:12 +0100

    Duplicate the test file

--- script/create-v2.rb
+++ script/create-v2.rb
@@ -12,3 +12,5 @@ raise "answer name missing from arguments" unless answer_name
 
 FileUtils.cp("lib/flows/#{answer_name}.rb", "lib/flows/#{answer_name}-v2.rb")
 FileUtils.cp("lib/flows/locales/en/#{answer_name}.yml", "lib/flows/locales/en/#{answer_name}-v2.yml")
+test_name = answer_name.gsub("-", "_")
+FileUtils.cp("test/integration/flows/#{test_name}_test.rb", "test/integration/flows/#{test_name}_v2_test.rb")
