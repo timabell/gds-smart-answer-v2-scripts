4bec4164b72f84fa4bbaaa3b717f3882fb540ed0
Tim Abell <tim@timwise.co.uk>
2013-09-03 17:26:13 +0100

    Script duplicating the rb file

---
+++ script/create-v2.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# ruby script create temporary duplicates of smart answers
+# These are used to allow testing and fact checking on preview
+# but will not be served on live (due to the status of draft)
+
+# usage: ruby scripts/create-v2.rb smart-answer-name
+
+require 'fileutils'
+
+answer_name = ARGV[0]
+
+FileUtils.cp("lib/flows/#{answer_name}.rb", "lib/flows/#{answer_name}-v2.rb")
