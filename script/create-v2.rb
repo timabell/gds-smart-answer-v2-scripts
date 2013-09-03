9465d34bc798ff74d0a8b1e8d2d92eb1538cc0e0
Tim Abell <tim@timwise.co.uk>
2013-09-03 22:20:37 +0100

    Refactor - use new load/write for yml and test

--- script/create-v2.rb
+++ script/create-v2.rb
@@ -5,8 +5,6 @@
 
 # usage: ruby scripts/create-v2.rb smart-answer-name
 
-require 'fileutils'
-
 def v2file(source, dest, &block)
   content = File.read(source)
   File.open(dest, "w") do |file|
@@ -21,12 +19,23 @@ raise "answer name missing from arguments" unless answer_name
 rb_file_original = "lib/flows/#{answer_name}.rb"
 rb_file_v2 = "lib/flows/#{answer_name}-v2.rb"
 
-FileUtils.cp("lib/flows/locales/en/#{answer_name}.yml", "lib/flows/locales/en/#{answer_name}-v2.yml")
+yml_file_original = "lib/flows/locales/en/#{answer_name}.yml"
+yml_file_v2 = "lib/flows/locales/en/#{answer_name}-v2.yml"
+
 test_name = answer_name.gsub("-", "_")
-FileUtils.cp("test/integration/flows/#{test_name}_test.rb", "test/integration/flows/#{test_name}_v2_test.rb")
+test_file_original = "test/integration/flows/#{test_name}_test.rb"
+test_file_v2 = "test/integration/flows/#{test_name}_v2_test.rb"
 
 v2file rb_file_original, rb_file_v2 do |content|
   content.gsub(/^status \:published$/, 'status :draft')
+end
+
+v2file yml_file_original, yml_file_v2 do |content|
+  content
+end
+
+v2file test_file_original, test_file_v2 do |content|
+  content
 end
 
 # change class names
