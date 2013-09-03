8b1b897c6056da543bff0eb98dd100c8940c7ab6
Tim Abell <tim@timwise.co.uk>
2013-09-03 20:00:00 +0100

    plan - comments

--- script/create-v2.rb
+++ script/create-v2.rb
@@ -10,7 +10,12 @@ require 'fileutils'
 answer_name = ARGV[0]
 raise "answer name missing from arguments" unless answer_name
 
+# duplicate files
 FileUtils.cp("lib/flows/#{answer_name}.rb", "lib/flows/#{answer_name}-v2.rb")
 FileUtils.cp("lib/flows/locales/en/#{answer_name}.yml", "lib/flows/locales/en/#{answer_name}-v2.yml")
 test_name = answer_name.gsub("-", "_")
 FileUtils.cp("test/integration/flows/#{test_name}_test.rb", "test/integration/flows/#{test_name}_v2_test.rb")
+
+# set to draft
+
+# change class names
